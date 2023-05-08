5f2f31f1d51e72997a03e96d55eb9bd3e5c2b65b
91hongppie <91hongppie@gmail.com>
2023-05-08 22:30:11 +0900

    TIL_swift: add split view and navigation view

---
+++ FaceIt/FaceIt/EmotionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EmotionViewController.swift
+//  FaceIt
+//
+//  Created by Kyuhong Jo on 2023/05/08.
+//
+
+import UIKit
+
+class EmotionViewController: UIViewController
+{
+    private let emotionalFaces: Dictionary<String,FacialExpression> = [
+        "angry": FacialExpression(eyes: .Closed, eyeBrows: .Furrowed, mouth: .Frown),
+        "happy": FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile),
+        "worried": FacialExpression(eyes: .Open, eyeBrows: .Relaxed, mouth: .Smirk),
+        "mischievious": FacialExpression(eyes: .Open, eyeBrows: .Furrowed, mouth: .Grin)
+    ]
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destinationvc = segue.destination
+        if let navcon = destinationvc as? UINavigationController {
+            destinationvc = navcon.visibleViewController ?? destinationvc
+        }
+        if let facevc = destinationvc as? FaceViewController {
+            if let identifier = segue.identifier {
+                if let expression = emotionalFaces[identifier] {
+                    facevc.expression = expression
+                    if let sendingButton = sender as? UIButton {
+                        facevc.navigationItem.title = sendingButton.currentTitle
+                    }
+                }
+            }
+        }
+    }
+
+}
