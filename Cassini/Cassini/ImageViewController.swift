5223b34150c231788773bd3108e0f60a9b85f827
91hongppie <91hongppie@gmail.com>
2023-05-16 14:50:57 +0900

    TIL_swift: add Cassini

---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Kyuhong Jo on 2023/05/16.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+    var imageURL: NSURL?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
