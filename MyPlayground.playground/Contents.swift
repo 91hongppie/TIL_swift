027ed50b1dd40af69a1cd2b6a667e6f03b632dcb
91hongppie <91hongppie@gmail.com>
2023-06-09 10:23:02 +0900

    add playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+//print("Hello World")
+/*
+ 우와
+ 주석 여러줄 쌉가능이다
+ */
+
+// print문 안에 코드를 넣는 것도 가능하다.
+print("Hello \(2+3) World")
+
+print("The result of 5 + 3 = \(5+3)")
+
+var a = 5
+var b = 8
+
+var c = a
+a = b
+b = c
+
+print("The value of a is \(a)")
+print("The value of b is \(b)")
