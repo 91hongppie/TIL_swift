2df7963cf1501f513598f84042caa4d5ee9472f3
91hongppie <91hongppie@gmail.com>
2023-06-13 21:21:11 +0900

    TIL_swift: learn play sound file, deep dive function

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -28,3 +28,12 @@ var numbers = [45, 73, 195, 53]
 var computedNumbers = [numbers[0] * numbers[1], numbers[1] * numbers[2], numbers[2] * numbers[3]]
 
 print(computedNumbers)
+
+
+func greeting() {
+    for _ in 0...3 {
+        print("hello")
+    }
+}
+
+greeting()
--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I got Pressed")
+        playSound(sender.currentTitle)
     }
     
-    
-
+    func playSound(_ resource: String?) {
+        let url = Bundle.main.url(forResource: resource, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
