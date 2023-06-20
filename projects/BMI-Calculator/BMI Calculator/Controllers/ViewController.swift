0a639fa676c43dd6c5d89dce0a9edd97c1bdf55f
91hongppie <91hongppie@gmail.com>
2023-06-20 09:47:58 +0900

    TIL_swift: processing Project BMI_Calculator & learn Class

--- projects/BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
+++ projects/BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        height.text = "\(String(format: "%.1f", sender.value))m"
+    }
+
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weight.text = "\(String(format: "%.0f", sender.value))Kg"
+    }
+    
+    @IBAction func calcluatedPressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+    }
+    
 
 }
 
