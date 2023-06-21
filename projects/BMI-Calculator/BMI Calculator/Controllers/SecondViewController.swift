6aff39627bececf255b15bf044081cd540a4c47f
91hongppie <91hongppie@gmail.com>
2023-06-21 18:20:39 +0900

    TIL_swift: add SecondViewController on BMI-Calculator

---
+++ projects/BMI-Calculator/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Kyuhong Jo on 2023/06/21.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
