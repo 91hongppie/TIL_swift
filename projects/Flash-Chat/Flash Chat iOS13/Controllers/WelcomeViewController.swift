eb2a25c34c523d607c81568ef7f165612ce73dd7
91hongppie <91hongppie@gmail.com>
2023-06-30 16:50:58 +0900

    TIL_swift: create Typing Animations, Timers and For Loops

--- projects/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ projects/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,7 +15,17 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-       
+        titleLabel.text = ""
+        var charIndex = 0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print(charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
     
 
