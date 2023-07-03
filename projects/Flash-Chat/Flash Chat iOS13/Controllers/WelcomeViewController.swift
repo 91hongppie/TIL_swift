0c80e749687d67cac6568fd7df0be28d0c4f4ca4
91hongppie <91hongppie@gmail.com>
2023-07-03 10:14:16 +0900

    TIL_swift: learn how to register user through firebase/auth

--- projects/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ projects/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -6,18 +6,23 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
-
-import CLTypingLabel
+import UIKit
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = "⚡️FlashChat"
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in self.titleLabel.text?.append(letter)}
+            charIndex += 1
+        }
     }
     
     
