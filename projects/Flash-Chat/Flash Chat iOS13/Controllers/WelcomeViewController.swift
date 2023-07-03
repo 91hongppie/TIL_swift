b2525cc004cb85c0d37ba5ac863d9254d1204bf0
91hongppie <91hongppie@gmail.com>
2023-07-03 09:09:44 +0900

    TIL_swift: apply package & compare Package Managers

--- projects/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ projects/Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -6,27 +6,20 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
-import UIKit
+
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = ""
-        var charIndex = 0
-        let titleText = "⚡️FlashChat"
-        for letter in titleText {
-            print(charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "⚡️FlashChat"
     }
+    
     
 
 }
