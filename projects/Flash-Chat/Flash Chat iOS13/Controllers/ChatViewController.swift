67e94b5a256ac14b92170a70a1761183fac014f7
91hongppie <91hongppie@gmail.com>
2023-07-05 23:35:23 +0900

    TIL_swift: complete learn how to use TableViewCell

--- projects/Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ projects/Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -25,7 +25,7 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
-        tableView.register(UINib(nibName: <#T##String#>, bundle: <#T##Bundle?#>), forCellReuseIdentifier: <#T##String#>)
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -49,8 +49,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
         return cell
     }
 }
--- projects/Flash-Chat/Flash Chat iOS13/Views/MessageCell.swift
+++ projects/Flash-Chat/Flash Chat iOS13/Views/MessageCell.swift
@@ -17,7 +17,8 @@ class MessageCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
