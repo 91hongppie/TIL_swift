abace5e6de9a0ee9ece9a1a91bcae071d65d3856
91hongppie <91hongppie@gmail.com>
2023-07-07 19:58:00 +0900

    TIL_swift: learn how to create keyboard avoiding view

--- projects/Flash-Chat/Flash Chat iOS13/AppDelegate.swift
+++ projects/Flash-Chat/Flash Chat iOS13/AppDelegate.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 import Firebase
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -19,8 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let db = Firestore.firestore()
         
-        print(db)
-        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+
         return true
     }
 
