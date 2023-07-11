b5a7adf248a8a11c8af933a60030f35d762112fe
91hongppie <91hongppie@gmail.com>
2023-07-11 22:04:39 +0900

    TIL_swift: complete H4X0R Project && run macos

---
+++ projects/H4X0R News/H4X0R News/Views/DetailView.swift
@@ -0,0 +1,25 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Kyuhong Jo on 2023/07/11.
+//
+
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
