909e6c6ceafa0cac25971ce70e86d6d13d259b4a
91hongppie <91hongppie@gmail.com>
2023-07-11 20:27:06 +0900

    TIL_swift: add H4X0R Project

---
+++ projects/H4X0R News/H4X0R News/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Kyuhong Jo on 2023/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
