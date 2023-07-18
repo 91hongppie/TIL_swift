ec29bf2a0d4e6e6e9500a1198e1bc84bd86376ad
91hongppie <91hongppie@gmail.com>
2023-07-18 23:30:40 +0900

    TIL_swift: learn how to create relationship graphs in Core Data

---
+++ projects/Todoey/Todoey/Views/CategoryViewController.swift
@@ -0,0 +1,96 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Kyuhong Jo on 2023/07/18.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categoryArray = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadCategories()
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategories() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from database \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: - Add New Categories
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            if let result = textField.text {
+                let newCategory = Category(context: self.context)
+                newCategory.name = result
+                self.categoryArray.append(newCategory)
+                
+                self.saveCategories()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        let category = categoryArray[indexPath.row]
+        
+        cell.textLabel?.text = category.name
+        
+        return cell
+    }
+}
