df6e3b2db471108db318df98e8448653101c9c3f
91hongppie <91hongppie@gmail.com>
2023-07-22 19:40:48 +0900

    TIL_swift: learn how to updating data from Realm

--- projects/Todoey/Todoey/Controllers/ToDoListViewController.swift
+++ projects/Todoey/Todoey/Controllers/ToDoListViewController.swift
@@ -66,9 +66,17 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
      
         if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving done status, \(error)")
+            }
             
         }
         
+        tableView.reloadData()
 //        context.delete(itemArray[indexPath.row])
 //        itemArray.remove(at: indexPath.row)
         
