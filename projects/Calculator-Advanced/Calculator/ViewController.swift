1902a4acd92659486ff7fd9b49ee557abdc7aac2
91hongppie <91hongppie@gmail.com>
2023-07-27 12:59:46 +0900

    TIL_swift: add Calculator-Advanced project

---
+++ projects/Calculator-Advanced/Calculator/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Angela Yu on 10/09/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var isFinishedTypingNumber: Bool = true
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text! += numValue
+            }
+        }
+        
+    }
+    
+}
+
