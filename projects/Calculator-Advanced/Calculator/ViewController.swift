ee7ca2855d1a0977caef9adf186ba56e02ea2510
91hongppie <91hongppie@gmail.com>
2023-07-28 21:55:00 +0900

    TIL_swift: learn how to dealing with the decimal place

--- projects/Calculator-Advanced/Calculator/ViewController.swift
+++ projects/Calculator-Advanced/Calculator/ViewController.swift
@@ -44,6 +44,18 @@ class ViewController: UIViewController {
                 displayLabel.text = numValue
                 isFinishedTypingNumber = false
             } else {
+                if numValue == "." {
+                    
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double!")
+                    }
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text! += numValue
             }
         }
