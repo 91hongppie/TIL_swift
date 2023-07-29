9685727600b485955e6720990b0e2310d5dfda65
91hongppie <91hongppie@gmail.com>
2023-07-29 20:21:03 +0900

    TIL_swift: learn about touple in swift

---
+++ language/Touple.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+let touple1 = ("Angela", 12)
+
+print(touple1.0)
+
+let touple2 = (name: "Angela", age: 12)
+
+print(touple2.name)
+
+let touple3: (name: String, age: Int)
+touple3 = (name: "Angela", age: 21)
+
+print(touple3.name)
