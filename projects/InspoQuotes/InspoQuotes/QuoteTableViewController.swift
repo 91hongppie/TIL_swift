f1f2a274bf89b5647a1082ce5957b9e47bc89f82
91hongppie <91hongppie@gmail.com>
2023-07-30 22:10:53 +0900

    InspoQuotes: create an inspirational Quotes app

--- projects/InspoQuotes/InspoQuotes/QuoteTableViewController.swift
+++ projects/InspoQuotes/InspoQuotes/QuoteTableViewController.swift
@@ -40,25 +40,21 @@ class QuoteTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return quotesToShow.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
 
         // Configure the cell...
-
+        cell.textLabel?.text = quotesToShow[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
