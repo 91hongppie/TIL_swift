20d684e5c59ba112198c3f140f24525391d663be
91hongppie <91hongppie@gmail.com>
2023-08-06 10:25:16 +0900

    Dice-Game: init Dice Game & create storyboard

---
+++ projects/DiceGame/DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Kyuhong Jo on 2023/08/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
