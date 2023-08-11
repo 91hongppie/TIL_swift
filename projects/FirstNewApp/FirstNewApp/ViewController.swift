cab1a12e23dc7650a05700a91bff4d2ba3eb1b88
91hongppie <91hongppie@gmail.com>
2023-08-11 19:57:24 +0900

    FirstNewApp: connect between main.storyboard and viewController

--- projects/FirstNewApp/FirstNewApp/ViewController.swift
+++ projects/FirstNewApp/FirstNewApp/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
+    }
+    
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요"
     }
 
+
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
 
 }
 
