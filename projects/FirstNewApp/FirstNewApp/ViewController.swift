5015bcbe7726032585028592a18db43aac1744c6
91hongppie <91hongppie@gmail.com>
2023-08-12 18:38:20 +0900

    FirstNewApp: add timer

--- projects/FirstNewApp/FirstNewApp/ViewController.swift
+++ projects/FirstNewApp/FirstNewApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,10 @@ class ViewController: UIViewController {
     
     let step: Float = 5
     
+    weak var timer: Timer?
+    
+    var number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,20 +27,44 @@ class ViewController: UIViewController {
     }
     
     func configureUI() {
+        number = 0
         mainLabel.text = "초를 선택하세요"
         slider.value = 0.5
+        timer?.invalidate()
     }
 
 
     
     @IBAction func sliderChanged(_ sender: UISlider) {
+        
         let seconds = Int(sender.value * 60)
         mainLabel.text = "\(seconds) 초"
-        
+        number = seconds
     }
 
     @IBAction func startButtonTapped(_ sender: UIButton) {
         // 1초씩 지나갈때마다 무언가를 실행
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [self] _ in
+            
+            
+            if number > 0 {
+                number -= 1
+                slider.value = Float(number) / Float(60)
+                mainLabel.text = "\(number) 초"
+            } else {
+                number = 0
+                timer?.invalidate()
+                configureUI()
+                AudioServicesPlayAlertSound(SystemSoundID(1322))
+            }
+            
+            
+            
+            
+            
+        }
+     
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
@@ -45,6 +74,5 @@ class ViewController: UIViewController {
         configureUI()
     }
     
-
 }
 
