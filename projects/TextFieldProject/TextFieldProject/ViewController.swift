b88d05318ad90f688c09c47d6a61fab6354055b9
91hongppie <91hongppie@gmail.com>
2023-08-13 15:11:52 +0900

    TextFieldProject: update textfield style

--- projects/TextFieldProject/TextFieldProject/ViewController.swift
+++ projects/TextFieldProject/TextFieldProject/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
+    }
+    
+    func setup() {
+        // view는 viewController에 해당하는 화면
+        view.backgroundColor = .gray
+        
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .go
     }
 
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
 
 }
 
