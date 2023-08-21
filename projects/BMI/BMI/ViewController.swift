d65af9c34ddce431e68c3354355598b918963aa7
91hongppie <91hongppie@gmail.com>
2023-08-21 21:35:29 +0900

    BMI: init project & WIP UI

---
+++ projects/BMI/BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Kyuhong Jo on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
