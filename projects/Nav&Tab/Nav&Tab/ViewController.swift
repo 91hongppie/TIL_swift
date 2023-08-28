351a02296891357a93d2f94da3e53fbd33b40ced
91hongppie <91hongppie@gmail.com>
2023-08-28 20:09:42 +0900

    Nav&Tab: learn make navbar & tabbar with storyboard

---
+++ projects/Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by Kyuhong Jo on 2023/08/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
