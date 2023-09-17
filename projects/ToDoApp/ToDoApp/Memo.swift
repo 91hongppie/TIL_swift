c448d38c4cae1c5dab7fc67a4254d7556f89be91
91hongppie <91hongppie@gmail.com>
2023-09-17 18:49:04 +0900

    add project for learn about how to use coreData

---
+++ projects/ToDoApp/ToDoApp/Memo.swift
@@ -0,0 +1,14 @@
+//
+//  Memo.swift
+//  ToDoApp
+//
+//  Created by Kyuhong Jo on 2023/09/17.
+//
+
+import Foundation
+
+struct Memo {
+    var memoText: String?
+    var date: Date?
+    var color: Int
+}
---
+++ projects/ToDoApp/ToDoApp/MemoData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MemoData+CoreDataClass.swift
+//  ToDoApp
+//
+//  Created by Kyuhong Jo on 2023/09/17.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MemoData)
+public class MemoData: NSManagedObject {
+
+}
