64c504edf6d39da103b8618fa9bfaa5b40cbaad4
91hongppie <91hongppie@gmail.com>
2023-09-18 22:33:42 +0900

    add code for ToDoApp

---
+++ projects/ToDoApp/ToDoApp/Models/ToDoData+CoreDataProperties.swift
@@ -0,0 +1,33 @@
+//
+//  ToDoData+CoreDataProperties.swift
+//  ToDoApp
+//
+//  Created by Kyuhong Jo on 2023/09/18.
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoData> {
+        return NSFetchRequest<ToDoData>(entityName: "ToDoData")
+    }
+
+    @NSManaged public var memoText: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var color: Int64
+
+    var dateString: String? {
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "yyyy-MM-dd"
+        guard let date = self.date else { return "" }
+        let savedDateString = myFormatter.string(from: date)
+        return savedDateString
+    }
+}
+
+extension ToDoData : Identifiable {
+
+}
