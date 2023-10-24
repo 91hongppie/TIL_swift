7f011bd05ed1fcd541102563b0fe751c6c2c3654
91hongppie <91hongppie@gmail.com>
2023-10-24 17:49:15 +0900

    modify directory & init FireChat

---
+++ projects/clone/FireChat/FireChat/Controller/ConversationsController.swift
@@ -0,0 +1,22 @@
+//
+//  ConversationsController.swift
+//  FireChat
+//
+//  Created by Kyuhong Jo on 10/24/23.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    // MARK: - Helpers
+}
