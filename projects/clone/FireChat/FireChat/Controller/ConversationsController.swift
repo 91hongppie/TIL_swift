51796f12c45a57fc4a3c17b102eb1332c9de6973
91hongppie <91hongppie@gmail.com>
2023-10-24 17:55:49 +0900

    add navigationController

--- projects/clone/FireChat/FireChat/Controller/ConversationsController.swift
+++ projects/clone/FireChat/FireChat/Controller/ConversationsController.swift
@@ -15,8 +15,23 @@ class ConversationsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    @objc func showProfile() {
+        print(123)
     }
     
     // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Messages"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(showProfile))
+    }
 }
